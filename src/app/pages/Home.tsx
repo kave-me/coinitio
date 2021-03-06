f733d50e39f43b51f4ec267b00d3ac14f02d6b99
Kave Mohammadi <kave.mdi@gmail.com>
2021-03-07 00:20:33 +0330

    refactor Home.tsx Page

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,12 +1,39 @@
-import { Button } from '@material-ui/core';
+import { Divider, Grid, makeStyles } from '@material-ui/core';
+import NavCard from '../../components/card/NavCard';
+import TradingViewChart from '../../components/chart/TradingViewChart';
+import SearchToken from '../../components/input/SearchToken';
+import TokenList from '../../components/list/TokenList';
+import Footer from '../../components/footer/Footer';
+
+const useStyles = makeStyles(() => ({
+  body: {
+    width: '100%',
+  },
+  container: {
+    marginTop: '10px',
+    // background: 'linear-gradient(180deg,rgba(0,0,0,0) 80%, rgba(255,255,255,.2) 20%)',
+    boxShadow: '1px 24px 58px 26px rgba(0,0,0,0.64)',
+    overflow: 'hidden',
+    borderRadius: '20px',
+  },
+}));
 
 export default function Home() {
+  const styles = useStyles();
   return (
-    <>
-      <h1>this is home</h1>
-      <Button variant="contained" color="primary" size="large">
-        by
-      </Button>
-    </>
+    <div className={styles.body}>
+      <NavCard />
+      <Grid container>
+        <Grid item xs={2} />
+        <Grid item className={styles.container} xs={8}>
+          <TradingViewChart />
+          <SearchToken />
+          <Divider />
+          <TokenList />
+        </Grid>
+        <Grid item xs={2} />
+        <Footer />
+      </Grid>
+    </div>
   );
 }
