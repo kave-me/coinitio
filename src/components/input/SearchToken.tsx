1619ffd9d096354bd6adc75f678e4bcc0ab9cac3
Kave Mohammadi <kave.mdi@gmail.com>
2021-03-07 00:16:39 +0330

    add SearchToken.tsx Component

---
+++ src/components/input/SearchToken.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      padding: '2px 4px',
+      display: 'flex',
+      alignItems: 'center',
+      width: '50%',
+      height: '4rem',
+      margin: '0 auto 3rem auto',
+    },
+    input: {
+      marginLeft: theme.spacing(1),
+      flex: 1,
+    },
+    iconButton: {
+      padding: 10,
+    },
+  }),
+);
+
+export default function SearchToken() {
+  const classes = useStyles();
+
+  return (
+    <Paper component="form" className={classes.root}>
+      <InputBase
+        className={classes.input}
+        placeholder="Search Token"
+        inputProps={{ 'aria-label': 'search token' }}
+      />
+      <IconButton type="submit" className={classes.iconButton} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+}
